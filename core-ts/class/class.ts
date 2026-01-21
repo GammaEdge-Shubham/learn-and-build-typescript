fe4128962aa5fe2b81dbb0d0a9708a4b0c36db06
Shubham Kumar Gupta <shubham.kumar@gammaedge.io>
2026-01-21 11:51:08 +0530

    learn: how we can declare and use class in ts

---
+++ core-ts/class/class.ts
@@ -0,0 +1,14 @@
+class Product {
+  id: number;
+  name: string;
+  price: number;
+
+  constructor(id: number, name: string, price: number) {
+    this.id = id;
+    this.name = name;
+    this.price = price;
+  }
+}
+
+const p1 = new Product(1, "Shirt", 200);
+console.log(p1);
